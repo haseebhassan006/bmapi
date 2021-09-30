658a9027184e4416cada70ea436b70b8c75340cf
HaseebHassan <mhhassan095@gmail.com>
2021-09-30 10:50:06 -0700

    migration created

---
+++ database/migrations/2021_09_30_173412_create_songs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('songs');
+            $table->integer('category_id');
+            $table->integer('singer_id');
+            $table->string('album_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
